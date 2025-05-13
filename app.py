2f7df745398ed6e5a43e0f8a48a33f0fc9172a0d
Oscar  Neyra Nazarrett <oscarneyra@MacBook-Air-9.local>
2025-05-12 22:57:32 -0700

    Add basic fall detection

--- app.py
+++ app.py
@@ -16,12 +16,24 @@ st.sidebar.markdown("Secure your home with AI surveillance.")
 st.title("📹 Surveillance Dashboard")
 st.markdown("Upload footage to detect falls or intruders. Alerts will be shown here when AI is enabled.")
 
+import tempfile
+from fall_detector import detect_fall
+
 # Upload section
 st.header("🔍 Upload a Home Security Video")
 uploaded_file = st.file_uploader("Choose a video", type=["mp4", "avi", "mov"])
 if uploaded_file:
-    st.success("Video uploaded!")
     st.video(uploaded_file)
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_video:
+        temp_video.write(uploaded_file.read())
+        result = detect_fall(temp_video.name)
+
+    if result:
+        st.error("⚠️ Fall Detected!")
+    else:
+        st.success("✅ No Fall Detected.")
+
 
 # Placeholder for alerts
 st.markdown("### ⚠️ Detected Events")
